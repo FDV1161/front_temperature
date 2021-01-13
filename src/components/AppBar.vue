4018231791698d78f3816b1bdbc079bbd9642d88
Фишер Дмитрий Владимирович <fisherdv@uriit.ru>
2021-01-13 12:23:19 +0500

    Добавлены иконки~

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,11 +1,16 @@
 <template>
   <v-app-bar app :color="color" clipped-left>
     <v-toolbar flat :color="color">
-      <v-toolbar-title>Title</v-toolbar-title>
+      <v-toolbar-title>
+        <span>Title</span>
+      </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items>
-        <v-btn text> Chart </v-btn>
-        <v-btn text> NotChart </v-btn>
+        <v-btn text> 
+            <v-icon left>mdi-chart-bell-curve-cumulative</v-icon>
+            <span class="text-capitalize"> Chart </span>            
+        </v-btn>
+        <v-btn text class="text-capitalize"> NotChart </v-btn>
       </v-toolbar-items>
     </v-toolbar>
   </v-app-bar>
@@ -13,6 +18,7 @@
 
 
 <script>
+
 export default {
   props: ["color"],
 };

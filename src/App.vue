d4f06152dd55fd3b9cfe9987a30990d9b79d2b2c
Фишер Дмитрий Владимирович <fisherdv@uriit.ru>
2021-01-13 17:55:15 +0500

    добавлен выпадающий nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,98 +1,80 @@
 <template>
   <v-app>
-    <!-- <v-app-bar 
+    <v-app-bar 
       app
       color=orange            
       clipped-left
-    /> -->
-    <AppBar :color=app_settings.app_color />
-    
-    <v-navigation-drawer 
+    />
+    <AppBar :color="app_settings.app_color" :drawer="drawer" />
+    <!-- https://codepen.io/kematzy/pen/zJmXwJ -->
+
+
+    <v-navigation-drawer
       app
       clipped
-      :color=app_settings.app_color
+      :color="app_settings.app_color"
+      v-model="drawer.side_bar"      
     >
-
-    <List />
-
+      <List />
     </v-navigation-drawer>
-    
 
-    <v-content 
-      app            
-    >
-    <v-container fluid fill-height>
-    <v-row align="center" justify="center">
-      <v-col align="center">
-        <v-sheet
-          color="white"
-          elevation="10"
-          height="142"
-          width="166"
-        >    
-      </v-sheet>  
-      </v-col>
-          
+    <v-content app>
+      <v-container fluid fill-height>
+        <v-row align="center" justify="center">
+          <v-col align="center">
+            <v-sheet color="white" elevation="10" height="142" width="166">
+            </v-sheet>
+          </v-col>
+        </v-row>
 
-    
-    </v-row>
-
-    <v-row>
-      <v-col align="center">        
-      <MyChart />
-      </v-col>
-    </v-row>
-
-    </v-container>
-
-    
-
-    
-
+        <v-row>
+          <v-col align="center">
+            <MyChart />
+          </v-col>
+        </v-row>
+      </v-container>
     </v-content>
 
-
-    <v-footer
-      app
-      color=green      
-      inset
-    >
+    <v-footer app color="green" inset>
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
     </v-footer>
-
-  
-  
-    
-  
-
-      
   </v-app>
 </template>
 
 
 <script>
 // import HelloWorld from './components/HelloWorld';
-import MyChart from "@/components/chart.vue"
-import List from "@/components/list.vue"
-import AppBar from "@/components/AppBar.vue"
+import MyChart from "@/components/chart.vue";
+import List from "@/components/list.vue";
+import AppBar from "@/components/AppBar.vue";
+
+// let default_side_bar = true;
+// if (window.innerWidth <= 1264) {
+//   default_side_bar = false;
+// }
+
+// console.log(window.innerWidth);
+
+
 
 export default {
-  name: 'App',
-
-
+  name: "App",
 
   components: {
     // HelloWorld,
     MyChart,
     List,
-    AppBar
+    AppBar,
   },
 
   data: () => ({
-    //
+    drawer: {
+      side_bar: window.innerWidth > 1264, // in order to not show after a reboots 
+      nav_bar: false
+    },
     app_settings: {
       app_color: "green",
-    }
+    },
   }),
 };
 </script>
--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,26 +1,86 @@
 <template>
   <v-app-bar app :color="color" clipped-left>
     <v-toolbar flat :color="color">
-      <v-toolbar-title>
-        <span>Title</span>
+      <v-btn icon @click="drawer.side_bar = !drawer.side_bar">
+        <v-icon> mdi-dots-vertical </v-icon>
+      </v-btn>
+
+      <v-toolbar-title class="text-uppercase">
+        <span class="font-weight-light">Title</span>
       </v-toolbar-title>
+
       <v-spacer></v-spacer>
-      <v-toolbar-items>
-        <v-btn text> 
-            <v-icon left>mdi-chart-bell-curve-cumulative</v-icon>
-            <span class="text-capitalize"> Chart </span>            
+      <!-- hidden-md-and-up <v-app-bar-nav-icon tile plain></v-app-bar-nav-icon> -->
+      <v-toolbar-items class="hidden-md-and-up">
+        <v-btn text @click="drawer.nav_bar = !drawer.nav_bar">
+          <v-app-bar-nav-icon disabled></v-app-bar-nav-icon>
+          <span class="text-capitalize"> Menu </span>
         </v-btn>
-        <v-btn text class="text-capitalize"> NotChart </v-btn>
+      </v-toolbar-items>
+
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn text>
+          <v-icon left>mdi-chart-bell-curve-cumulative</v-icon>
+          <span class="text-capitalize"> Charts </span>
+        </v-btn>
+        <v-btn flat text class="text-capitalize"> NotChart </v-btn>
+        <v-btn flat text class="text-capitalize"> LogIn </v-btn>
       </v-toolbar-items>
     </v-toolbar>
+
+    <v-dialog
+      v-model="drawer.nav_bar"
+      fullscreen
+      hide-overlay
+      transition="dialog-bottom-transition"
+    >
+      <v-card>
+        <v-toolbar flat :color="color">
+          <v-toolbar-title class="text-uppercase">
+            <span class="font-weight-light">Title</span>
+          </v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-btn icon @click.native="drawer.nav_bar = false">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-toolbar>
+
+        <v-list>
+          <v-list-item v-for="(item, i) in nav" :key="i">
+            <v-list-item-icon>
+              <v-icon>mdi-{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.text }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-card>
+    </v-dialog>
   </v-app-bar>
 </template>
 
 
 <script>
-
 export default {
-  props: ["color"],
+  props: ["color", "drawer"],
+
+  data: () => ({
+    nav: [
+      {
+        icon: "home",
+        text: "Home",
+        title: "Back to Home page",
+        active: true,
+      },
+      {
+        icon: "info",
+        text: "About",
+        title: "About this demo",
+        active: false,
+      },
+    ],
+  }),
 };
 </script>
 

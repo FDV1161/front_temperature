8ff030c2168b55d3672a11775253a4ad4d96fb59
Dmitriy <dmitriyfiser@gmail.com>
2021-05-27 15:33:19 +0500

    Поле для ввода вынесено в компонент

---
+++ src/components/Base/Fields/InputField.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-row>
+    <v-col>
+      <v-text-field
+        :value="value"
+        :label="label"
+        @input="(v) => $emit('input', v)"
+        :class="{ required: required }"
+        :rules="rules"
+        v-bind="$attrs"
+      >
+      </v-text-field>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  model: {
+    prop: "value",
+    event: "input",
+  },
+  props: {
+    value: String,
+    label: String,
+    required: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    rules() {
+      return [
+        ...(this.required == true
+          ? [(value) => !!value || "Обязательное поле"]
+          : []),
+      ];
+    },
+  },
+};
+</script>
+
+<style>
+.required label::before {
+  content: "* ";
+  color: red;
+}
+</style>
